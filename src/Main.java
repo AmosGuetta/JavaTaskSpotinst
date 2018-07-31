6f2576ff8f1365e6fe6d086c4d7fe221ee8a3929
Amos Guetta <amos.guetta.1990@gmail.com>
2018-07-31 11:46:00 +0300

    Tests

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try{
+            fileSystem fileSystem = new fileSystem();
+
+
+            fileSystem.addDirectory("root","Desktop");
+            fileSystem.addDirectory("root","Development");
+            fileSystem.addDirectory("Desktop","Tests");
+            fileSystem.addDirectory("Development","myFirstApp");
+            fileSystem.addDirectory("Development","JavaProjects");
+            fileSystem.addDirectory("Development","C_Projects");
+
+            fileSystem.addFile("Desktop","StudentProfilePicture",100);
+            fileSystem.addFile("Desktop","CV",90);
+            fileSystem.addFile("Tests","FileSystemTests",80);
+            fileSystem.addFile("Tests","DataStructuresTests",70);
+
+
+            fileSystem.addFile("myFirstApp","HelloWorld",60);
+            fileSystem.addFile("JavaProjects","Server",50);
+            fileSystem.addFile("C_Projects","clients",40);
+
+
+            //fileSystem.showFileSystem();
+
+
+            System.out.println("--------------------------------Delete--------------------------------------");
+
+            fileSystem.delete("FileSystemTests");
+            fileSystem.showFileSystem();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
